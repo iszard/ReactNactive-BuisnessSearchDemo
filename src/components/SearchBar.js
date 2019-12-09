2852f9545fd529de3b8b770f4f9556abb304f38e
GElrick\HPC <gelrick@parlaygroup.com>
2019-12-09 00:26:53 -0500

    First Commit - Mid Development

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { View, TextInput, StyleSheet } from 'react-native';
+import { Feather } from '@expo/vector-icons';
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: '#F0EEEE',
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: 'row',
+  },
+  inputStyle: {
+    fontSize: 18,
+    flex: 1,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: 'center',
+    marginHorizontal: 10,
+  },
+});
+
+const SearchBar = () => (
+        <View style={styles.backgroundStyle} >
+            <Feather name="search" style={styles.iconStyle} />
+            <TextInput
+                style={styles.inputStyle}
+                placeholder="Search"
+            />
+        </View>
+);
+
+export default SearchBar;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import SearchBar from '../components/SearchBar';
+
+const styles = StyleSheet.create({
+
+});
+
+const SearchScreen = () => (
+        <View>
+            <SearchBar />
+            <Text>Search Screen</Text>
+        </View>
+);
+
+export default SearchScreen;
